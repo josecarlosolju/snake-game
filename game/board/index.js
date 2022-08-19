a19a27fa387eafbe78deda98442dff939967dcb2
José Carlos <josecarlos@unipam.edu.br>
2022-08-18 21:07:03 -0300

    gerando comida em posição aleatoria

--- game/board/index.js
+++ game/board/index.js
@@ -2,6 +2,9 @@ const BOARD_SIZE =21;
 
 export const gameboard = document.getElementById('game-board')
 
-function generateRandomBoardPosition() {
-
+export function generateRandomBoardPosition() {
+    return {
+       x: Math.floor(Math.random() * BOARD_SIZE) + 1,
+       y: Math.floor(Math.random() * BOARD_SIZE) + 1, 
+    }
 }
